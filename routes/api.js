22b69bcf927c8d079e7c97064eca3427f5e93653
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-13 01:50:06 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { cekKey } = require('../database/db'); 
-const { youtubePlay, youtubeMp4, youtubeMp3, pinterest, otakudesu, igstalk, igstory, igdownloader, twitterdownloader } = require('../controllers/yt');
+const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, ptl, motivasi, indonesia, malaysia, thailand, vietnam, korea, japan, naruto, china, tiktok, asupan, geayubi, ukhty, rikagusriani, anony, hijaber, joker, harley, cecan, santuy, bocil } = require('../controllers/randomtext');
 
 router.get('/checkkey', async (req, res) => {
