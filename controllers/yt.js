954d11a361e6ad17af58398ef34e45773033ba1b
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-13 01:00:04 +0700

    update yt.js

--- controllers/yt.js
+++ controllers/yt.js
@@ -1,4 +1,5 @@
 const { ytPlay, ytMp3, ytMp4 } = require("../lib/youtube");
+const { otakudesu, pinterest } = require("../lib/scrapt");
 const { cekKey } = require('../database/db');
 
 async function youtubePlay(req, res) {
@@ -60,6 +61,58 @@ async function youtubeMp4(req, res) {
         message: `apikey ${apikey} not found, please register first!`
     });
     ytMp4(url).then(result => {
+        res.status(200).send({
+            status: 200, 
+            result: result
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+async function otakudesu(req, res) {
+    const judul = req.query.judul;
+    const apikey = req.query.apikey;
+    if (url === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    otakudesu(judul).then(result => {
+        res.status(200).send({
+            status: 200, 
+            result: result
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+async function pinterest(req, res) {
+    const querry = req.query.querry;
+    const apikey = req.query.apikey;
+    if (url === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    pinterest(querry).then(result => {
         res.status(200).send({
             status: 200, 
             result: result
